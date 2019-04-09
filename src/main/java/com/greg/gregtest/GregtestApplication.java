7c3369d2e53a0afc84a48dd1fb325dc428a21982
greg.lebreton <root@localhost.localdomain>
2019-04-09 07:52:31 -0700

    commit test

---
+++ src/main/java/com/greg/gregtest/GregtestApplication.java
@@ -0,0 +1,13 @@
+package com.greg.gregtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GregtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GregtestApplication.class, args);
+	}
+
+}
